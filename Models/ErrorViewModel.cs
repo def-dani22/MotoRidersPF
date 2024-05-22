7f7f02148ce1e9471061b0c53cf73210a273ac5b
def-dani22 <esteban.gomez.rivera@cuc.cr>
2024-05-21 21:33:10 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MotoRidersPF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
